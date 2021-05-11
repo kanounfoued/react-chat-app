61cdfce8fcb09243204dd3a238c4c8d7f3c01fd5
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-05-11 02:25:57 +0100

    :alien: fetching the list of contacts

--- src/store/action/messenger/contact/index.js
+++ src/store/action/messenger/contact/index.js
@@ -5,65 +5,18 @@ import {
   ADD_CONTACT,
   UPDATE_CONTACT,
 } from "../../../type";
-import image1 from "../../../../assets/contact/1.jpg";
-import image2 from "../../../../assets/contact/2.jpg";
-import image3 from "../../../../assets/contact/3.jpg";
-import image4 from "../../../../assets/contact/4.jpg";
-import image5 from "../../../../assets/contact/5.jpg";
-import image6 from "../../../../assets/contact/6.jpg";
-import image7 from "../../../../assets/contact/7.jpg";
+import { customAxios } from "../../../../api/api";
 
-const mockData = [
-  {
-    id: "sldfskjfoeirjfier",
-    name: "mohamed foued",
-    familyName: "kanoun",
-    picture: image1,
-  },
-  {
-    id: "sdmlfkeporkf",
-    name: "said",
-    familyName: "baklioun",
-    picture: image2,
-  },
-  {
-    id: "sufnknvxlqs",
-    name: "amine",
-    familyName: "samrani",
-    picture: image3,
-  },
-  {
-    id: "lek,flzr,mzmze",
-    name: "khelifi",
-    familyName: "mohamed",
-    picture: image4,
-  },
-  {
-    id: "sd,qmdsfq,fzef,",
-    name: "raouf",
-    familyName: "hamdine",
-    picture: image5,
-  },
-  {
-    id: "zel,mzlr,zmlr,tzerk,ùslf",
-    name: "noufel",
-    familyName: "mohamed",
-    picture: image6,
-  },
-  {
-    id: "ùfgmfmobkzlenlknlaze",
-    name: "mohamed",
-    familyName: "kacem",
-    picture: image7,
-  },
-];
-
-export const fetchContactList = () => (dispatch) => {
+export const fetchContactList = () => async (dispatch) => {
   try {
+    const { data: contacts } = await customAxios().get(
+      `https://random-data-api.com/api/users/random_user?size=6`
+    );
+
     dispatch({
       type: FETCH_MESSENGER_CONTACT_LIST,
       payload: {
-        contacts: mockData,
+        contacts,
       },
     });
   } catch (error) {
